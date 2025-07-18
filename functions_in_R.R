24db6ffec990ed93f6ba8e0871e96b4a9ee3d6bb
Prem Vaniya <113230199+premvaniya@users.noreply.github.com>
2025-07-18 22:20:13 +0530

    Update functions_in_R.R

--- functions_in_R.R
+++ functions_in_R.R
@@ -11,3 +11,27 @@ volcylinder = function(dia=5, len=100){
 }
 volcylinder(7,200)
 volcylinder()
+
+
+
+#mimo(multiple input and multiple output)
+calculate_stats <- function(x, y) {
+  sum_result <- x + y
+  diff_result <- x - y
+  prod_result <- x * y
+  
+  return(list(sum = sum_result, difference = diff_result, product = prod_result))
+}
+
+# Test
+results <- calculate_stats(10, 3)
+print(results$sum)        # 13
+print(results$difference) # 7
+print(results$product)    # 30
+
+
+#inline function
+numbers <- list(1, 2, 3, 4, 5)
+
+squared <- lapply(numbers, function(x) x^2)
+print(squared)
